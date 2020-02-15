0681d92b204b9db5ac1d3a0537d2f72fec80ff63
RomaIIIka <romses2000@mail.ru>
2020-02-15 11:07:13 +0300

    some fix

--- lab18/index.js
+++ lab18/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const handler = require('./handler');
-
+const PORT = 5000;
 let server = http.createServer();
 server.on('request', handler);
-server.listen(5000);
+server.listen(PORT);
+console.info(`Server listen on port ${PORT}`);
