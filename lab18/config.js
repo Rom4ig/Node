bb56b92ee0e681ac5c87a0341fd165e7fae32afc
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2020-02-26 09:29:37 +0300

    Update config.js

--- lab18/config.js
+++ lab18/config.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-module.exports = new Sequelize("databasenane", "user", "password", {
+module.exports = new Sequelize("databasename", "user", "password", {
     dialect: "mssql",
     host: "localhost",
     define: {
